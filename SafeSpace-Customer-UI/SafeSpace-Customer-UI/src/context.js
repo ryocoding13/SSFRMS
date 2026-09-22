9c8f008c5c434eaff76890917ba48adb265d896f
Vector <giathang2k3@gmail.com>
2026-09-22 12:07:48 +0700

    Customer UI: dựng lại toàn bộ giao diện khách hàng theo Figma

--- SafeSpace-Customer-UI/SafeSpace-Customer-UI/src/context.js
+++
@@ -1,3 +0,0 @@
-import { createContext, useContext } from "react";
-export const CustomerContext = createContext(null);
-export const useCustomer = () => useContext(CustomerContext);
--- SafeSpace-Customer-UI/SafeSpace-Customer-UI/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import CustomerApp from "./CustomerApp";
-
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <CustomerApp />
-  </React.StrictMode>,
-);
